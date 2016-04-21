02d68af05165786dcd43c49fc3ea0ce78a65532d
Nicola Pedretti <oeran93@gmail.com>
2016-04-20 23:23:52 -0500

    basic implementation

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,22 @@
 (function ($) {
-	$.fn.sS = function () {
-		this.fadeOut();
+	$.fn.sS = function (options) {
+	
+		options = $.extend({
+			path: './',
+			images: [],
+			showTime: 5000,
+			bgRepeat: 'no-repeat'
+		}, options);
+
+		$(this).css('background-repeat',options.bgRepeat);
+		$(this).css('-webkit-transition','background-image 2s');
+		
+		(function slide (container, slideNumber) {
+			$(container).css('background-image',"url('"+options.path+options.images[slideNumber]+"')");
+			setTimeout(function () {
+				slide(container,(++slideNumber)%options.images.length);
+			}, options.showTime);
+		})(this, 0);
+
 	}
 })(jQuery);
