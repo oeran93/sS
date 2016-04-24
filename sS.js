d4c0dbb37c7e09fa3d67e9f57e3437ccf1b50912
Nicola Pedretti <np.pedretti@gmail.com>
2016-04-23 23:42:21 -0500

    restructuring code in functions

--- sS.js
+++ sS.js
@@ -1,22 +1,33 @@
 (function ($) {
-	$.fn.sS = function (options) {
 	
-		options = $.extend({
-			path: './',
-			images: [],
-			showTime: 5000,
-			bgRepeat: 'no-repeat'
-		}, options);
-
-		$(this).css('background-repeat',options.bgRepeat);
-		$(this).css('-webkit-transition','background-image 2s');
-		
+	$.fn.sS = function (options) {
+		$.fn.sS.options = $.extend($.fn.sS.options, options);
+		$.fn.sS.generalCss(this);
+		$.fn.sS[$.fn.sS.options['animation']](this);
 		(function slide (container, slideNumber) {
-			$(container).css('background-image',"url('"+options.path+options.images[slideNumber]+"')");
+			$(container).css('background-image',"url('"+$.fn.sS.options.path+$.fn.sS.options.images[slideNumber]+"')");
 			setTimeout(function () {
-				slide(container,(++slideNumber)%options.images.length);
-			}, options.showTime);
-		})(this, 0);
-
+				slide(container,(++slideNumber)%$.fn.sS.options.images.length);
+			}, $.fn.sS.options.showTime);
+		})(this, 1);
 	}
+
+	$.fn.sS.options = {
+		path: './',
+		images: [],
+		showTime: 5000,
+		bgRepeat: 'no-repeat',
+		animation: 'transition'
+	}
+
+	$.fn.sS.generalCss = function (container) {
+		$(this).css('background-repeat',$.fn.sS.options.bgRepeat);
+	}
+
+	$.fn.sS.transition = function (container) {
+		$(container).css('transition','background-image 4s');
+		$(container).css('-webkit-transition','background-image 4s');
+		$(container).css('-moz-transition','background-image 4s');
+	}
+
 })(jQuery);
